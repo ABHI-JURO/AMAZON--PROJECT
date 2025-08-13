e8272ecf30bdaf91450b877add6a56a6943d2e09
Abhishek Shrivastava <138434193+ABHI-JURO@users.noreply.github.com>
2025-08-13 12:54:13 +0530

    remove default tests and added money tests

---
+++ AMAZON-PROJECT/tests-jasmine/moneyTest.js
@@ -0,0 +1,15 @@
+import { formalCurrency } from '../scripts/utils/money.js';
+
+describe('twst suite: formalCurrency', () => {
+    it('converts cents into dollers', () => {
+        expect(formalCurrency(2095)).toEqual('20.95');
+    });
+
+    it('works with 0', () => {
+        expect(formalCurrency(0)).toEqual('0.00');
+    });
+
+    it('rounds up to the nearest cent', () => {
+        expect(formalCurrency(2000.5)).toEqual('20.01');
+    });
+});

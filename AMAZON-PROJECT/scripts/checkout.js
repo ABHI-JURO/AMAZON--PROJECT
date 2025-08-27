47b93ade07815913090d382588d8f76312cce3e9
Abhishek Shrivastava <138434193+ABHI-JURO@users.noreply.github.com>
2025-08-27 13:38:45 +0530

    add backend practice file

--- AMAZON-PROJECT/scripts/checkout.js
+++ AMAZON-PROJECT/scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummery } from "./checkout/orderSummery.js";
 import { renderPaymentSummery } from "./checkout/paymentSummery.js";
 //import "../data/cart-class.js";
+import '../data/backend-practice.js';
 
 renderOrderSummery();
 renderPaymentSummery();
